4b247a12d55a7b500e775866eb9e052f60c2557d
unknown <blandinedjoumsie@gmail.com>
2024-08-18 12:47:26 +0200

    added a list,modified it, then checked its length

---
+++ variable-list.py
@@ -0,0 +1,13 @@
+grocery_list = ["milk", "sugar", "chocolate", "honey"]
+temperature_list = [20.4, -12.8 ]
+
+print(grocery_list)
+#print(len(grocery_list))
+#print(temperature_list)
+
+grocery_list[3] = "beer"
+#print(grocery_list[4])
+print(grocery_list)
+grocery_list.append("butter")
+print(grocery_list)
+
