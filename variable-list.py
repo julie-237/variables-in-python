849f088fb961c2954c46b0a63f263cf43d78378d
unknown <blandinedjoumsie@gmail.com>
2024-08-18 14:46:45 +0200

    added a new list , validated it using conditions

--- variable-list.py
+++ variable-list.py
@@ -1,5 +1,18 @@
 grocery_list = ["milk", "sugar", "chocolate", "honey"]
+fruit_list = ["banana", "apple", "orange","mango"]
 temperature_list = [20.4, -12.8 ]
+
+client_order = input("which fruit do you want ?")
+if client_order not in fruit_list:
+    print("we don't have " + client_order)
+    exit()
+    
+    print("ok")
+
+"""if client_order in fruit_list:
+    print("you can get it")
+else:
+    print("we don't have " + client_order)"""
 
 print(grocery_list)
 #print(len(grocery_list))
@@ -10,4 +23,7 @@ grocery_list[3] = "beer"
 print(grocery_list)
 grocery_list.append("butter")
 print(grocery_list)
+
+if "milk" in grocery_list:
+    print("ok")
 
