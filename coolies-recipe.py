4f607fa38b2426e495f634c46596786e919223f7
unknown <blandinedjoumsie@gmail.com>
2024-08-19 14:15:16 +0200

    added a new file, named it 'coolies-recipe' then added a list of ingredients in it and some conditions to a guess game

---
+++ coolies-recipe.py
@@ -0,0 +1,31 @@
+import time
+ingredient_list=["chocolate", "milk", "flour", "sugar" ]
+score = 0
+print("Hello! you have to guess 3 ingredients from the cookies recipe")
+time.sleep(3)
+ingredient_1= input("guess ingredient 1 ")
+if ingredient_1 in ingredient_list:
+    print("wow, congratulations")
+    score += 1
+else:
+    print("oups, error!")
+time.sleep(2)
+
+ingredient_2= input("guess ingredient 2 ")
+if ingredient_2 in ingredient_list:
+    print("wow, congratulations")
+    score = score + 1
+else:
+    print("oups, error!")
+time.sleep(2)
+    
+ingredient_3= input("guess ingredient 3 ")
+if ingredient_3 in ingredient_list:
+    print("wow, congratulations")
+    score = score + 1
+else:
+    print("oups, error!")
+    
+time.sleep(2)
+print("Game over! you have " + str(score) + "points")
+    
