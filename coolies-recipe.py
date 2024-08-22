22dcdf554ffe8dff9a191ee0a4e6fc0dd32c4707
unknown <blandinedjoumsie@gmail.com>
2024-08-22 04:31:51 +0200

    added loops to my code

--- coolies-recipe.py
+++ coolies-recipe.py
@@ -1,38 +1,26 @@
 import time
 ingredient_list=["chocolate", "milk", "flour", "sugar" ]
+response_sheet=[ ]
 score = 0
 print("Hello! you have to guess 3 ingredients from the cookies recipe")
 time.sleep(3)
-ingredient_1= input("guess ingredient 1 ")
-if ingredient_1 in ingredient_list:
-    print("wow, congratulations")
-    score += 1
-else:
-    print("oups, error!")
-time.sleep(2)
 
-ingredient_2= input("guess ingredient 2 ")
-if ingredient_2 in ingredient_list:
-    if ingredient_2 != ingredient_1:
-        print("wow, congratulations")
-        score = score + 1
+for i in range(3):
+    i +=1
+    ingredient= input("guess ingredient  " + str(i))
+    if ingredient in ingredient_list:
+        if ingredient not in  response_sheet :
+            print("wow, congratulations")
+            score = score + 1
+            response_sheet.append(ingredient)
+        else:
+            print("repeated answer!!!")
     else:
-        print("oups, error!")
-time.sleep(2)
-    
-ingredient_3= input("guess ingredient 3 ")
-if ingredient_3 in ingredient_list:
-    if ingredient_3 != ingredient_1 and ingredient_2:
-        print("wow, congratulations")
-        score = score + 1
-    else:
-        print("oups, error!")
+         print("oups, error!")
+        
     
 time.sleep(2)
-if score <= 1:
-    print("Game over! you have " + str(score) + " point")
-else:
-    print("Game over! you have " + str(score) + " points")
+print("Game over! you have " + str(score) + " points") if score > 1 else print("Game over! you have " + str(score) + "point")
 
 print("The ingredient_list was:")
 time.sleep(2)
